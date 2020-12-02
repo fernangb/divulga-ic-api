26eb3b60ccbd26c855b71360f7cdacbdc146af7f
fernangb <gfsouza@poli.ufrj.br>
2020-12-01 21:07:08 -0300

    Criação de model de curso

--- src/routes/cursos.routes.ts
+++ src/routes/cursos.routes.ts
@@ -1,9 +1,18 @@
 import {Router} from 'express';
+import Curso from '../models/Curso';
 
 const cursosRouter = Router();
 
+const cursos: Curso[] = [];
+
 cursosRouter.post('/', (request, response) => {
-  return response.json({message: 'Hello'});
-})
+  const {nome, predio, endereco} = request.body;
+
+  const curso = new Curso(nome, predio, endereco);
+
+  cursos.push(curso);
+
+  return response.json(curso);
+});
 
 export default cursosRouter;
