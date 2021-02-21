961ec81716931c9e7533d1a60386bd18406e11f8
fernangb <gfsouza@poli.ufrj.br>
2021-02-20 23:46:51 -0300

    Atualização do Perfil

---
+++ src/modules/usuarios/services/UpdatePerfilService.spec.ts
@@ -0,0 +1,118 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import UpdatePerfilService from './UpdatePerfilService';
+import FakeUsuariosRepository from '../repositories/fakes/FakeUsuariosRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+let fakeUsuariosRepository: FakeUsuariosRepository;
+let fakeHashProvider: FakeHashProvider;
+let updatePerfil: UpdatePerfilService;
+
+describe('UpdatePerfil', () => {
+  beforeEach(() => {
+    fakeUsuariosRepository = new FakeUsuariosRepository();
+    fakeHashProvider = new FakeHashProvider();
+    updatePerfil = new UpdatePerfilService(
+      fakeUsuariosRepository,
+      fakeHashProvider,
+    );
+  });
+
+  it('deve ser possivel atualizar um perfil de usuário', async () => {
+    const usuario = await fakeUsuariosRepository.create({
+      email: 'fulano@detal.com',
+      senha: '123456',
+      id_nivel: '1',
+      nome: 'Teste',
+    });
+
+    const usuarioAtualizado = await updatePerfil.execute({
+      id_usuario: usuario.id,
+      nome: 'Novo Teste',
+      email: 'novoFulano@detal.com',
+    });
+
+    expect(usuarioAtualizado?.nome).toBe('Novo Teste');
+    expect(usuarioAtualizado?.email).toBe('novoFulano@detal.com');
+  });
+
+  it('não deve ser possível atualizar a senha de um outro email de usuário', async () => {
+    await fakeUsuariosRepository.create({
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: '123456',
+      id_nivel: '1',
+    });
+
+    const user = await fakeUsuariosRepository.create({
+      nome: 'Test',
+      email: 'test@example.com',
+      senha: '123456',
+      id_nivel: '1',
+    });
+
+    await expect(
+      updatePerfil.execute({
+        id_usuario: user.id,
+        nome: 'John Doe',
+        email: 'johndoe@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('deve ser possível atualizar a senha', async () => {
+    const user = await fakeUsuariosRepository.create({
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: '123456',
+      id_nivel: '1',
+    });
+
+    const usuarioAtualizado = await updatePerfil.execute({
+      id_usuario: user.id,
+      nome: 'John Trê',
+      email: 'johntre@example.com',
+      senha_antiga: '123456',
+      senha: '123123',
+    });
+
+    expect(usuarioAtualizado?.senha).toBe('123123');
+  });
+
+  it('não deve ser possível atualizar a senha sem a senha antiga', async () => {
+    const usuario = await fakeUsuariosRepository.create({
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: '123456',
+      id_nivel: '1',
+    });
+
+    await expect(
+      updatePerfil.execute({
+        id_usuario: usuario.id,
+        nome: 'John Trê',
+        email: 'johntre@example.com',
+        senha: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('não deve ser possível atualizar a senha com a senha entiga errada', async () => {
+    const user = await fakeUsuariosRepository.create({
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: '123456',
+      id_nivel: '1',
+    });
+
+    await expect(
+      updatePerfil.execute({
+        id_usuario: user.id,
+        nome: 'John Trê',
+        email: 'johntre@example.com',
+        senha_antiga: 'wrong-old-password',
+        senha: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/usuarios/services/UpdatePerfilService.ts
@@ -0,0 +1,75 @@
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import Usuario from '../infra/typeorm/entities/Usuario';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsuariosRepository from '../repositories/IUsuariosRepository';
+
+interface IRequest {
+  id_usuario: string;
+  nome: string;
+  email: string;
+  senha?: string;
+  senha_antiga?: string;
+}
+
+@injectable()
+class UpdatePerfilService {
+  constructor(
+    @inject('UsuariosRepository')
+    private usuariosRepository: IUsuariosRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    id_usuario,
+    nome,
+    email,
+    senha,
+    senha_antiga,
+  }: IRequest): Promise<Usuario | undefined> {
+    const usuario = await this.usuariosRepository.encontrarPeloId(id_usuario);
+
+    if (!usuario) {
+      throw new AppError('Usuário não encontrado');
+    }
+
+    const usuarioComEmailAtualizado = await this.usuariosRepository.encontrarPeloEmail(
+      email,
+    );
+
+    if (
+      usuarioComEmailAtualizado &&
+      usuarioComEmailAtualizado.id !== id_usuario
+    ) {
+      throw new AppError('Email já cadastrado no sistema');
+    }
+
+    usuario.nome = nome;
+    usuario.email = email;
+
+    if (senha && !senha_antiga) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para atualizá-la',
+      );
+    }
+
+    if (senha && senha_antiga) {
+      const verificarSenhaAntiga = await this.hashProvider.compararHash(
+        senha_antiga,
+        usuario.senha,
+      );
+
+      if (!verificarSenhaAntiga) {
+        throw new AppError('Senha antiga inválida');
+      }
+
+      usuario.senha = await this.hashProvider.gerarHash(senha);
+    }
+
+    return this.usuariosRepository.save(usuario);
+  }
+}
+
+export default UpdatePerfilService;
