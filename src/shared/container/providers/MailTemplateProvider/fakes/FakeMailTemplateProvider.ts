68c516f874b629517f5ed7f0c99b694f184bd0cb
fernangb <gfsouza@poli.ufrj.br>
2021-02-22 15:03:58 -0300

    Remoção de testes unitários

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++
@@ -1,9 +0,0 @@
-import IMailTemplateProvider from '../models/IMailTemplateProvider';
-
-class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse(): Promise<string> {
-    return 'Mail';
-  }
-}
-
-export default FakeMailTemplateProvider;
